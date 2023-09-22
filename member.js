32f36a258168027fb87e7c974d3b46cc7eec5d41
Wolfgang Denthaner <wolfgang@denthaner.at>
2023-09-22 05:21:34 +0200

    Copilot second commit

--- member.js
+++ member.js
@@ -1,10 +1,12 @@
-function skillMember() {
-    var member = document.getElementById("member");
-    var memberValue = member.options[member.selectedIndex].value;
-    var memberText = member.options[member.selectedIndex].text;
-    if (memberValue == "member") {
-        document.getElementById("member").style.color = "#999";
-    } else {
-        document.getElementById("member").style.color = "#000";
-    }
+function skillMember(){
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '=',
+        }
+    };
 }
